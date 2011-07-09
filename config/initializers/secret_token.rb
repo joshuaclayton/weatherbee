e927804b4cb3c1803e9ad7e47655ae51bd4de635
Joshua Clayton <jclayton@thoughtbot.com>
2011-07-08 23:10:55 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weatherbee::Application.config.secret_token = 'aa56d033601b901f7433cbc49452756258227cbec38618f783cc1bd8fdf8135ad1c38ca2cd89f80d8f2ea99f82fa95faf0da036919c0ca9a35af34beaa48ea89'
